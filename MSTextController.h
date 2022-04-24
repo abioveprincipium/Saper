9902207ec6834f6de3aafc29bb7807885db05f78
Alicja Grądys <263696@student.pwr.edu.pl>
2022-04-24 17:55:54 +0000

    Upload MSTextController.h

---
+++ MSTextController.h
@@ -0,0 +1,15 @@
+#ifndef CPP_MSTEXTCONTROLLER_H
+#define CPP_MSTEXTCONTROLLER_H
+#include "MineSweeperBoard.h"
+#include "MSBoardTextView.h"
+
+class MSTextController:  public MinesweeperBoard{
+    MinesweeperBoard &board;
+    MSBoardTextView &view;
+public:
+    MSTextController(MinesweeperBoard &board, MSBoardTextView &view);
+    MSTextController();
+    void play();
+};
+
+#endif //CPP_MSTEXTCONTROLLER_H
